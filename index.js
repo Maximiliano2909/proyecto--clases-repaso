8ad78173e7a2f1b937198ecec5336a0ffce7c447
jorge fernandez <max1-29@hotmail.com>
2023-02-13 15:14:20 -0300

    index.html-index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hola javascript")
+//mi primer comentario
+/*muchos renglones*/
+//let vs cons
+let contador1 = 0
+let contador2 = 3
+let contador3 = 2 
+console.log(contador2*contador3*4);
+
+
+const string1 = 'hola'
+string1 ='Aqui'
+const string2 = 'como estas'
+const string3 = '?'
+console.log(string+""+string2+string3);
