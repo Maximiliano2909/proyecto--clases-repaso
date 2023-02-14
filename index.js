9c6cd682a0d95f498cc1167f1001cf76e604518a
jorge fernandez <max1-29@hotmail.com>
2023-02-13 23:35:35 -0300

    cambios en index.js

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ console.log("hola javascript")
 //mi primer comentario
 /*muchos renglones*/
 //let vs cons
-let contador1 = 0
+let contador = 0
+let contador1= 1
 let contador2 = 3
 let contador3 = 2 
 console.log(contador2*contador3*4);
 
 
-const string1 = 'hola'
-string1 ='Aqui'
-const string2 = 'como estas'
-const string3 = '?'
-console.log(string+""+string2+string3);
+const string1 = "hola"
+const string2 = "como estas"
+const string3 = "?"
+console.log(string1+" "+string2+string3);
+
+const nombreUsuario = prompt("Ingresa tu nombre");
+alert("Bienvenido"+nombreUsuario)
+
+const ingresaEmail = prompt("ingresa tu email")
+alert("Gracias" +ingresaEmail)
